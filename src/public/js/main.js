48d2c6d4e33ba511e1333319add7da3ef34a15a3
VaalentinoB <valentinoburioni@hotmail.com>
2024-06-17 19:11:34 -0300

    sweet configuration

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -6,3 +6,29 @@ socket.emit("mensaje", "Hola Mundo, te escribo desde el cliente");
 let usuario;
 
 const ChatBox = document.getElementById("ChatBox");
+
+Swal.fire({
+  title: "Identificate",
+
+  input: "text",
+
+  text: "Ingresa un usuario para identificarte en el Chat",
+
+  inputValidator: (value) => {
+    return !value && "Necesitas escribir un nombre para continuar";
+  },
+
+  allowOutsideClick: false,
+}).then((result) => {
+  usuario = result.value;
+});
+
+chatBox.addEventListener("keyup", (event) => {
+  if (event.key === "Enter") {
+    if (chatBox.value.trim().length > 0) {
+      socket.emit("mensaje", { usuario: usuario, mensaje: chatBox.value });
+
+      chatBox.value = "";
+    }
+  }
+});
